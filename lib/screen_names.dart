e6885587446c1086a749bb4f6bd2e52857e93c18
Zachary Gonzalez <zacharygonzalez1234@gmail.com>
2022-07-11 17:58:34 -0700

    Screen Names Added

---
+++ lib/screen_names.dart
@@ -0,0 +1,10 @@
+class ScreeNames {
+  static String adminSignIn = '/AdminSignIn';
+  static String createEvent = '/CreateEvent';
+  static String eventCheckIn = '/EventCheckIn';
+  static String eventInfo = '/Events/Event';
+  static String findEvent = '/Events';
+  static String welcome = '/';
+  static String studentSignInScreen = '/StudentSignIn';
+  static String qrCodeScreen = '/QrCode';
+}
