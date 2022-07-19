468c2afe52c9d9999f930b5504369f4b932fe433
Zachary Gonzalez <zacharygonzalez1234@gmail.com>
2022-07-19 08:21:29 -0700

    Admin Sign In Screen

--- lib/Screens/admIn_sign_in.dart
+++ lib/Screens/admIn_sign_in.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:unlv_ceao_mobile_sign_in/Widgets/unlv_app_bar.dart';
+import 'package:unlv_ceao_mobile_sign_in/Widgets/unlv_button.dart';
+import 'package:unlv_ceao_mobile_sign_in/Widgets/unlv_textfield.dart';
 
 class AdminSignUpScreen extends StatefulWidget {
   const AdminSignUpScreen({Key? key}) : super(key: key);
@@ -9,13 +11,47 @@ class AdminSignUpScreen extends StatefulWidget {
 }
 
 class _AdminSignUpScreenState extends State<AdminSignUpScreen> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
   @override
   Widget build(BuildContext context) {
- return Scaffold(
-      appBar: UNLVAppBar(),
+    return Scaffold(
+      appBar: UNLVAppBar(
+        text: 'Admin Sign In',
+      ),
       body: ListView(
         children: [
-          
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                UNLVTextfield(
+                  hintText: 'Email',
+                  verticalPadding: MediaQuery.of(context).size.height * 0.025,
+                  horizontalPadding: MediaQuery.of(context).size.height * 0.025,
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  controller: email,
+                ),
+                UNLVTextfield(
+                  hintText: 'Password',
+                  verticalPadding: MediaQuery.of(context).size.height * 0.025,
+                  horizontalPadding: MediaQuery.of(context).size.height * 0.025,
+                  borderWidth: 1,
+                  borderRadius: 10,
+                  isObscure: true,
+                  controller: password,
+                ),
+                UNLVButton(
+                  ontap: () {},
+                  width: MediaQuery.of(context).size.width,
+                  horizontalPadding: MediaQuery.of(context).size.height * 0.025,
+                  text: 'Sign In',
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
