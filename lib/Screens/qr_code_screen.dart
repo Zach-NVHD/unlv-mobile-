104fdb25ef22f3fa9921d7daa7c0f45e11176801
Zachary Gonzalez <zacharygonzalez1234@gmail.com>
2022-07-19 19:43:19 -0700

    Qr Code Screen Done

--- lib/Screens/qr_code_screen.dart
+++ lib/Screens/qr_code_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
 import 'package:unlv_ceao_mobile_sign_in/Widgets/unlv_app_bar.dart';
+import 'package:unlv_ceao_mobile_sign_in/constants.dart';
 
 class QRCodeScreen extends StatefulWidget {
   const QRCodeScreen({Key? key}) : super(key: key);
@@ -11,11 +13,41 @@ class QRCodeScreen extends StatefulWidget {
 class _QRCodeScreenState extends State<QRCodeScreen> {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-      appBar: UNLVAppBar(),
+    return Scaffold(
+      appBar: UNLVAppBar(
+        text: 'Check In',
+      ),
       body: ListView(
         children: [
-          
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Constants.text(fontSize: 18, text: '''
+ID sign in successful.
+Using the QR Code provided below, an event organizer will let you in shortly.
+'''),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+          Padding(
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.1),
+            child: Material(
+              elevation: 10,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.38,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: PrettyQr(
+                    data: 'https://google.com',
+                    size: 50,
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
