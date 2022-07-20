96c577563a433626b797fb0612d804dc2d7f01c6
Zachary Gonzalez <zacharygonzalez1234@gmail.com>
2022-07-20 10:03:26 -0700

    Event Class Made

---
+++ lib/Database/Scemas/event.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Event {
+  Event(
+      {this.title = '',
+      this.description = '',
+      required this.start,
+      required this.end,
+      this.imageUrl = '',
+      this.isActive = false,
+      this.location = ''});
+
+  String title;
+  String description;
+  DateTime start;
+  DateTime end;
+  String location;
+  String imageUrl;
+  Image? image;
+  bool isActive;
+
+  String toJson() {
+    Map<String, dynamic> map = {
+      'title': title,
+      'description': description,
+      'start': Timestamp.fromDate(start),
+      'end': Timestamp.fromDate(end),
+      'location': location,
+      'isActive': isActive,
+      'imageUrl': imageUrl
+    };
+    return jsonEncode(map);
+  }
+
+  void fromJson(Map<String, dynamic> map) {
+    title = map['title'] ?? '';
+    description = map['description'] ?? '';
+    start = ((map['start'] ?? Timestamp.now()) as Timestamp).toDate();
+    end = ((map['end'] ?? Timestamp.now()) as Timestamp).toDate();
+    location = map['location'] ?? '';
+    isActive = map['isActive'] ?? false;
+    imageUrl = map['imageUrl'] ?? '';
+  }
+
+  @override
+  String toString() {
+    return '''
+    title: $title,
+    description: $description
+    start: $start
+    end: $end,
+    location: $location,
+    imageUrl: $imageUrl
+    isActive: $isActive
+    image: $image
+''';
+  }
+}
