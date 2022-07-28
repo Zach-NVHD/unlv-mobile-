f67222f8e9206d61c79d0f3470f8b0a2eb94e2bc
Zachary Gonzalez <zacharygonzalez1234@gmail.com>
2022-07-27 17:10:55 -0700

    Show Inactive Tiles On Admin

--- lib/Widgets/unlv_event_tile.dart
+++ lib/Widgets/unlv_event_tile.dart
@@ -16,55 +16,69 @@ class UNLVEventTile extends StatefulWidget {
 class _UNLVEventTileState extends State<UNLVEventTile> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: MediaQuery.of(context).size.width * 0.1,
-        vertical: MediaQuery.of(context).size.width * 0.05,
-      ),
-      child: GestureDetector(
-        onTap: (){
-          context.vxNav.push(Uri.parse(ScreeNames.eventInfo));
-        },
-        child: Material(
-          elevation: 10,
-          borderRadius: BorderRadius.circular(10),
-          child: Container(
-            height: MediaQuery.of(context).size.width * 0.65,
-            width: MediaQuery.of(context).size.width * 1.0,
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.005),
-                Container(
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                  child:  widget.event.image ?? Image.asset(
-                    'Images/template_tile_image.png',
-                    width: MediaQuery.of(context).size.width * 1.1,
-                    height: MediaQuery.of(context).size.width * 0.5,
-                    fit: BoxFit.fill,
+    return Stack(children: [
+      Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.1,
+          vertical: MediaQuery.of(context).size.width * 0.05,
+        ),
+        child: GestureDetector(
+          onTap: () {
+            context.vxNav.push(Uri.parse(ScreeNames.eventInfo));
+          },
+          child: Material(
+            elevation: 10,
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              height: MediaQuery.of(context).size.width * 0.65,
+              width: MediaQuery.of(context).size.width * 1.0,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.005),
+                  Container(
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                    child: widget.event.image ??
+                        Image.asset(
+                          'Images/template_tile_image.png',
+                          width: MediaQuery.of(context).size.width * 1.1,
+                          height: MediaQuery.of(context).size.width * 0.5,
+                          fit: BoxFit.fill,
+                        ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Constants.heading(text: widget.event.title),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child:
-                      Constants.text(text: widget.event.start.toString()),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Constants.text(text: widget.event.location),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Constants.heading(text: widget.event.title),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Constants.text(text: widget.event.start.toString()),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Constants.text(text: widget.event.location),
+                  )
+                ],
+              ),
             ),
           ),
         ),
       ),
-    );
+    widget.event.isActive ? const SizedBox() : Positioned.fill(
+          child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.1,
+          vertical: MediaQuery.of(context).size.width * 0.05,
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.grey[200]!.withOpacity(0.4),
+              borderRadius: BorderRadius.circular(10)),
+        ),
+      )),
+    ]);
   }
 }
